5b97d1ae65025bb9018cca298d6562510827a3c3
Patrik Sandahl <patrik.sandahl@gmail.com>
2018-06-23 09:06:05 +0200

    Skeleton code.

---
+++ src/AbstractGL.hs
@@ -0,0 +1,12 @@
+-- |
+-- Module: AbstractGL
+-- Copyright: (c) 2018 Patrik Sandahl
+-- Licence: BSD3
+-- Maintainer: Patrik Sandahl <patrik.sandahl@gmail.com>
+-- Stability: experimental
+-- Portability: portable
+--
+-- Entry module for the user API. The user API can be called from any thread.
+module AbstractGL
+    (
+    ) where
---
+++ src/AbstractGL/Backend.hs
@@ -0,0 +1,13 @@
+-- |
+-- Module: AbstractGL.Backend
+-- Copyright: (c) 2018 Patrik Sandahl
+-- Licence: BSD3
+-- Maintainer: Patrik Sandahl <patrik.sandahl@gmail.com>
+-- Stability: experimental
+-- Portability: portable
+--
+-- Entry module for the backend API. The backend API can only be called from the
+-- thread owning the OpenGL context.
+module AbstractGL.Backend
+    (
+    ) where
